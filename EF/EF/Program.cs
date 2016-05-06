653b4c520d697febc85cb427558304c49067d096
git <lionel.repellin@gmail.com>
2016-05-07 01:22:37 +0200

    Clean examples and add an item in two ways

--- EF/EF/Program.cs
+++ EF/EF/Program.cs
@@ -1,12 +1,10 @@
 ﻿using EF.DAL;
-using EF.DAL.Repositories;
-using EF.Domain;
-using EF.Domain.Borrowers;
+using EF.Domain.Catalogs;
 using EF.Domain.Items;
 using System;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace EF
 {
@@ -16,85 +14,69 @@ namespace EF
         {
             using (var ctx = new Context())
             {
-
-                
-                /*
+/*
                 var borrowers = ctx.Borrowers.ToList();
-                //Write<Borrower>(borrowers);
-
-                var items = ctx.Catalogs
-                                    .Where(c => c.Type == CatalogType.General)
-                                    .Select(c => c.Items.OfType<Book>())
-                                    .ToList();
-                                    */
-
-
-                //Console.WriteLine("".PadRight(30,'-'));
-                /*
+                Write(borrowers);
+                                
                 var availableItems = ctx.LibraryItems.Where(i => i.Status == ItemStatus.Available).ToList();
-                Write<LibraryItem>(availableItems);
-
+                Write(availableItems);
+                             
+                // get the first borrower
+                var borrower = ctx.Borrowers.Include(b => b.Loans).First();
+                Console.WriteLine("First borrower with loan: {0} {1}", borrower.Firstname, borrower.Lastname);
                 Console.WriteLine("".PadRight(30, '-'));
-                
-                //var borrower = ctx.Borrowers.Include(b => b.Loans).First();
-                */
 
-                //var borrowersCount = ctx.Database.SqlQuery<int>("SELECT COUNT(0) FROM emprunteur").First();
+                // count number of borrowers
+                var borrowersCount = ctx.Database.SqlQuery<int>("SELECT COUNT(0) FROM emprunteur").Single();
+                Console.WriteLine("Borrowers count : {0}", borrowersCount);
+                Console.WriteLine("".PadRight(30, '-'));
 
+                // find the first DVD
                 var dvd = ctx.LibraryItems.OfType<Dvd>().First();
+                Console.Write("First DVD: {0}", dvd.ShowProperties());
+                Console.WriteLine("".PadRight(30, '-'));
 
                 // use stored procedure
-                var legacyRepository = new LegacyRepository(ctx);
-                var borrowersWithDVD = legacyRepository.FindBorrowersWhoOwnsArticlesByType("DVD");
-                
-                var borrower = ctx.Borrowers
-                                    //.Include(b => b.Address)
-                                    .First();
-                
-                
-                /*
-                var borrowerFull = ctx.Loans
-                                        //.Include(a => a.LibraryItem)                    
-                                        .Where(l => l.BorrowerId == borrower.Id).First();
+                //                var legacyRepository = new LegacyRepository(ctx);
+                //                var borrowersWithDVD = legacyRepository.FindBorrowersWhoOwnsArticlesByType("DVD");
 
-                */
-                Console.WriteLine(borrower.ShowProperties());
-                /*
-                if(borrower.Loans != null && borrower.Loans.Count() > 0)
-                {
-                    var loan = borrower.Loans.First();
-                    Console.WriteLine(loan);
-
-                    if(loan.Borrower != null)
-                    {
-                        Console.WriteLine(loan.Borrower);
-                    }
-                }
-                */
-
-
-                //var book = ctx.LibraryItems.OfType<Book>().First();
-                //Console.WriteLine(book);
-
-                Console.WriteLine("".PadRight(30, '-'));
-                /*
+                var borrowerWithAddress = ctx.Borrowers
+                                            .Include(b => b.Address)
+                                            .First();
+                                                
                 var loans = ctx.Loans.ToList();
-                Write<Loan>(loans);
+                Write(loans);
+*/
 
-                Console.WriteLine("".PadRight(30, '-'));
-                */
+                // add a new book
+                var myBook = new Book {
+                    Author = "Victor Hugo",
+                    ISBN = "132132",
+                    Language = Language.French,
+                    Status = ItemStatus.Available,
+                    Title = "Les Misérables",
+                    Catalog = new Catalog { Type = CatalogType.General }
+                };
 
+                ctx.LibraryItems.Add(myBook);
+                ctx.SaveChanges();
+
+                // another way to add a new entity to the context is to change its state to "Added"
+                //ctx.Entry<Book>(myBook).State = EntityState.Added;
+                //ctx.SaveChanges();
             }
-            
+
             Console.Read();
         }
 
-        static void Write<T>(IEnumerable<T> objs)
+        static void Write(IEnumerable<object> objects)
         {
-            foreach(var o in objs)
+            foreach(var obj in objects)
             {
-                Console.WriteLine(o.ShowProperties());
+                Console.WriteLine(obj.ShowProperties());
             }
+
+            Console.WriteLine("".PadRight(30, '-'));
         }
     }
 }
